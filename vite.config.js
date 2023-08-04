b82b9ae545d134448ebbb5a5a74e12f5be348b26
AnderzRomero <andersonromerorico@gmail.com>
2023-08-04 11:09:05 -0500

    ajustes base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://anderzromero.github.io/',
+  base:'https://anderzromero.github.io/Entrega_Final_React',
 })
